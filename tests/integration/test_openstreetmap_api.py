1c6d5593ef8f228ff07b33fdf7a4ea64e485065f
mrMoish <@>
2026-08-18 16:18:44 -0300

    fix: correct OpenStreetMap test

--- tests/integration/test_openstreetmap_api.py
+++ tests/integration/test_openstreetmap_api.py
@@ -1,9 +1,9 @@
-from src.api import OpenStreetMap
+from src.openstreetmap_api import OpenStreetMap
 
 def test_openstreetmap_search():
     openstreetmap = OpenStreetMap()
-    result = openstreetmap.search('Moscow')
+    result = openstreetmap.search('Russia')
     assert isinstance(result, list)
     assert len(result) > 0
     first_result = result[0]
-    assert first_result['name'] == 'Москва'
+    assert first_result['name'] == 'Россия'
