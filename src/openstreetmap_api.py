ddeccc803123e3e2cd362409e04bbc1afb964a68
mrMoish <@>
2026-08-18 17:00:28 -0300

    chore: comply with black and flake8

--- src/openstreetmap_api.py
+++ src/openstreetmap_api.py
@@ -2,25 +2,33 @@ import requests
 from abc import ABC, abstractmethod
 from typing import cast
 
+
 class BaseApi(ABC):
 
     @abstractmethod
-    def get_data(self, url: str, params: dict[str, str], headers: dict[str, str]|None=None) -> list[dict[str, str]]:
-        ...
+    def get_data(
+        self, url: str, params: dict[str, str], headers: dict[str, str] | None = None
+    ) -> list[dict[str, str]]: ...
 
 
 class OpenStreetMap(BaseApi):
 
-    BASE_URL = 'https://nominatim.openstreetmap.org/'
+    BASE_URL = "https://nominatim.openstreetmap.org/"
 
-    def get_data(self, url: str, params: dict[str, str], headers: dict[str, str]|None=None) -> list[dict[str, str]]:
+    def get_data(
+        self, url: str, params: dict[str, str], headers: dict[str, str] | None = None
+    ) -> list[dict[str, str]]:
         response = requests.get(url, params=params, headers=headers)
         response.raise_for_status()
         data = response.json()
         return cast(list[dict[str, str]], data)
 
     def search(self, country: str) -> list[dict[str, str]]:
-        return self.get_data(f"{self.BASE_URL}search",
-                             params={"country": country,
-                                     'format': 'jsonv2',},
-                             headers= {"User-Agent": "sky-watch/1.0"})
+        return self.get_data(
+            f"{self.BASE_URL}search",
+            params={
+                "country": country,
+                "format": "jsonv2",
+            },
+            headers={"User-Agent": "sky-watch/1.0"},
+        )
--- tests/integration/test_openstreetmap_api.py
+++ tests/integration/test_openstreetmap_api.py
@@ -1,9 +1,10 @@
 from src.openstreetmap_api import OpenStreetMap
 
+
 def test_openstreetmap_search() -> None:
     openstreetmap = OpenStreetMap()
-    result = openstreetmap.search('Russia')
+    result = openstreetmap.search("Russia")
     assert isinstance(result, list)
     assert len(result) > 0
     first_result = result[0]
-    assert first_result['name'] == 'Россия'
+    assert first_result["name"] == "Россия"
